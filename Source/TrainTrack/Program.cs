f716e33b027d7885e7172bd7c385fd4fc68823d0
Pierre Nygård <pierre.nygard@gmail.com>
2020-03-10 01:33:45 +0100

    All minimum requirement classes added (from Data files). Trains, Stations, Passengers, TimeTables now read from files and populated into Lists in Program. Driver @pierre-nygard

--- Source/TrainTrack/Program.cs
+++ Source/TrainTrack/Program.cs
@@ -1,17 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Threading;
 
 namespace TrainTrack
 {
     class Program
     {
+        static List<Passenger> passengers;
+        static List<Train> trains;
+        static List<Station> stations;
+        static List<TimeTable> timeTables;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            FetchData();
+            Init.PrintHeader();
+
+            Console.WriteLine("The Railway Group 6");
+            Console.WriteLine("Passengers:");
+            Console.WriteLine("{0}\t{1}", "id", "fullName");
+            passengers.ForEach(p => {
+                Console.WriteLine("{0}\t{1}", p.ID, p.FullName);
+                Thread.Sleep(10);
+            });
+
+            Console.WriteLine("\nTrains:");
+            Console.WriteLine("{0}\t{1}\t\t\t{2}\t{3}", "id", "name", "speed", "operated");
+            trains.ForEach(t => {
+                Console.WriteLine("{0}\t{1}\t\t{2}\t{3}", t.ID, t.Name, t.Speed, t.Operated);
+                Thread.Sleep(80);
+            });
+
+            Console.WriteLine("\nStations:");
+            Console.WriteLine("{0}\t{1}\t\t\t{2}", "id", "name", "endStation");
+            stations.ForEach(s => {
+                Console.WriteLine("{0}\t{1}\t\t{2}", s.ID, s.Name.Substring(0,(s.Name.Length > 15) ? 15 : s.Name.Length), s.EndStation);
+                Thread.Sleep(80);
+            });
+
+            Console.WriteLine("\nTimeTables:");
+            Console.WriteLine("{0}\t{1}\t\t{2}\t{3}", "trainID", "stationID", "departureTime", "arrivalTime");
+            timeTables.ForEach(t => {
+                Console.WriteLine("{0}\t{1}\t\t\t{2}\t\t{3}", t.TrainID, t.StationID, t.DepartureTime, t.ArrivalTime);
+                Thread.Sleep(80);
+            });
+
+        }
+
+        /// <summary>
+        /// Reads all data from ./assets folder and populates static lists in Program
+        /// </summary>
+        static void FetchData()
+        {
+            passengers = Init.GetPassengers(Init.ReadFile(@"assets/passengers.txt"));
+            trains = Init.GetTrains(Init.ReadFile(@"assets/trains.txt"));
+            stations = Init.GetStations(Init.ReadFile(@"assets/stations.txt"));
+            timeTables = Init.GetTimeTables(Init.ReadFile(@"assets/timetable.txt"));
         }
     }
 
-    class Passenger
+    public class Passenger
     {
         private int _id;
         private string _fullName;
@@ -25,28 +76,51 @@ namespace TrainTrack
         }
     }
 
-    class Train
+    /// <summary>
+    /// Interface for any object that can be positioned inside a train tile item
+    /// </summary>
+    public interface ITileItem
+    {
+        // Not yet implemented
+        // Tile tile;
+    }
+
+    public class Train : ITileItem
     {
         private int _id;
         private string _name;
-        private int _maxSpeed;
+        private int _speed;
         private bool _operated;
 
         public int ID { get => _id; }
         public string Name { get => _name; }
-        public int MaxSpeed { get => _maxSpeed; }
+        public int Speed { get => _speed; }
         public bool Operated { get => _operated; }
 
-        public Train(int id, string name, int maxSpeed, bool operated)
+        public Train(int id, string name, int speed, bool operated)
         {
             _id = id;
             _name = name;
-            _maxSpeed = maxSpeed;
+            _speed = speed;
             _operated = operated;
+        }
+
+        public void DoStuff()
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                Console.WriteLine($"{this.Name} travelling {i}");
+                Thread.Sleep(this.Speed);
+            }
         }
     }
 
-    class Station
+    public class Route
+    {
+
+    }
+
+    public class Station : ITileItem
     {
         private int _id;
         private string _name;
@@ -64,23 +138,25 @@ namespace TrainTrack
         }
     }
 
-    class Timetable
+    public class Switch : ITileItem
     {
-        private int _id;
+
+    }
+
+    public class TimeTable
+    {
         private int _stationId;
         private int _trainId;
         private string _departureTime;
         private string _arrivalTime;
 
-        public int ID { get => _id; }
         public int StationID { get => _stationId; }
         public int TrainID { get => _trainId; }
         public string DepartureTime { get => _departureTime; }
         public string ArrivalTime { get => _arrivalTime; }
 
-        public Timetable(int id, int stationId, int trainId, string departureTime, string arrivalTime)
+        public TimeTable(int stationId, int trainId, string departureTime, string arrivalTime)
         {
-            _id = id;
             _stationId = stationId;
             _trainId = trainId;
             _departureTime = departureTime;
